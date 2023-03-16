63b8f9b4f92cf2c560444ba3b60e4b0a36b6b5ff
deeonwuli <onwulidumebi@gmail.com>
2023-03-16 23:25:53 +0100

    example with curl

--- src/scripts/exercise-two.ts
+++ src/scripts/exercise-two.ts
@@ -22,11 +22,7 @@ function main() {
             const { objects: dataSets } = await api.models.dataSets
                 .get({
                     fields: {
-                        id: true,
-                        name: true,
-                        displayName: true,
-                        indicators: true,
-                        periodType: true,
+                        $owner: true,
                     },
                     pageSize: 10,
                 })
@@ -49,14 +45,28 @@ function main() {
                 .getData();
 
             const ANC1CoverageIndicator = indicators[0] as Ref;
+            if (!ANC1CoverageIndicator) throw new Error("Indicator not found");
             const dataSetsWithANCIndicators = dataSets.map(dataSet => {
-                dataSet.indicators = [...dataSet.indicators, ANC1CoverageIndicator];
-                return dataSet;
+                return { ...dataSet, indicators: [...dataSet.indicators, ANC1CoverageIndicator] };
             });
 
+            console.debug({ dataSets: dataSetsWithANCIndicators });
             // post the new dataset value and log the response
             const response = await api.metadata.post({ dataSets: dataSetsWithANCIndicators }).getData();
             console.debug(response);
+
+            // exercise with curl
+            // get the first 10 datasets
+            // curl "https://admin:district@dev.eyeseetea.com/play/api/dataSets.json?fields=id,name,periodType&pageSize=10" | jq > datasets.json
+
+            // get ANC 1 Coverage indicator, add to each dataset in datasets.json file and remove pager object
+            // curl "https://admin:district@dev.eyeseetea.com/play/api/indicators.json?fields=id&filter=name:eq:ANC%201%20Coverage" | jq > ancIndicator.json
+
+            // post the new dataset value
+            // curl -H 'Content-Type: application/json' \
+            //   -d @datasets.json  \
+            //   -X POST \
+            //   https://admin:district@dev.eyeseetea.com/play/api/dataSets | jq > response.json
         },
     });
 
