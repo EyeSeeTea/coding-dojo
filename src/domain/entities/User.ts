a0e5bd78210284948b548b48a37290cb6e1ff4a3
p3rcypj <pablo@percypj.com>
2023-03-31 09:18:14 +0000

    Remove my User type and tests

--- src/domain/entities/User.ts
+++ src/domain/entities/User.ts
@@ -1,8 +1,8 @@
 import _ from "lodash";
 import { NamedRef } from "./Ref";
 
-interface UserProps {
-    id: number;
+export interface UserData {
+    id: string;
     name: string;
     username: string;
     userRoles: UserRole[];
@@ -10,97 +10,31 @@ interface UserProps {
     dateWhenDisabled?: Date;
 }
 
-// export interface UserData {
-//     id: string;
-//     name: string;
-//     username: string;
-//     userRoles: UserRole[];
-//     userGroups: NamedRef[];
-//     dateWhenDisabled?: Date;
-// }
-
-export class User {
-    private id: number;
-    private name: string;
-    public username: string;
-    private userRoles: UserRole[];
-    private userGroups: NamedRef[];
-    private dateWhenDisabled?: Date;
-
-    constructor(props: UserProps) {
-        const { id, name, username, userRoles, userGroups, dateWhenDisabled } = props;
-        this.id = id;
-        this.name = name;
-        this.username = username;
-        if (User.userRolesHaveAuthorityAll(userRoles) && dateWhenDisabled)
-            throw new Error("Admin user can't be disabled.");
-        this.userRoles = userRoles ?? [];
-        this.userGroups = userGroups ?? [];
-        if (dateWhenDisabled && new Date() < dateWhenDisabled)
-            throw new Error("Date when user was disabled can't be newer than the current date.");
-        this.dateWhenDisabled = dateWhenDisabled;
-    }
-
-    public disable(dateWhenDisabled?: Date): Date | boolean {
-        if (dateWhenDisabled && new Date() < dateWhenDisabled)
-            throw new Error("Date when user was disabled can't be newer than the current date.");
-        if (this.dateWhenDisabled) return this.dateWhenDisabled;
-        else if (!this.isAdmin()) {
-            this.dateWhenDisabled = dateWhenDisabled ?? new Date();
-            return this.dateWhenDisabled;
-        } else return this.dateWhenDisabled == undefined;
-    }
-
-    public enable(): boolean {
-        if (this.dateWhenDisabled) this.dateWhenDisabled = undefined;
-        return this.dateWhenDisabled == undefined;
-    }
-
-    public isDisabled(): boolean {
-        return this.dateWhenDisabled != undefined;
-    }
-
-    public daysDisabled(): number | undefined {
-        if (this.dateWhenDisabled) {
-            const diff = Math.abs(new Date().getTime() - this.dateWhenDisabled.getTime());
-            return Math.floor(diff / (1000 * 3600 * 24));
-        } else return this.dateWhenDisabled;
-    }
-
-    public isAdmin(): boolean {
-        return _.some(this.userRoles, ({ authorities }) => authorities.includes("ALL"));
-    }
-
-    private static userRolesHaveAuthorityAll(userRoles: UserRole[]): boolean {
-        return _.some(userRoles, ({ authorities }) => authorities.includes("ALL"));
-    }
-}
-
 export interface UserRole extends NamedRef {
     authorities: string[];
 }
 
-// export class User {
-//     public readonly id: string;
-//     public readonly name: string;
-//     public readonly username: string;
+export class User {
+    public readonly id: string;
+    public readonly name: string;
+    public readonly username: string;
 
-//     private readonly userGroups: NamedRef[];
-//     private readonly userRoles: UserRole[];
+    private readonly userGroups: NamedRef[];
+    private readonly userRoles: UserRole[];
 
-//     constructor(data: UserData) {
-//         this.id = data.id;
-//         this.name = data.name;
-//         this.username = data.username;
-//         this.userRoles = data.userRoles;
-//         this.userGroups = data.userGroups;
-//     }
+    constructor(data: UserData) {
+        this.id = data.id;
+        this.name = data.name;
+        this.username = data.username;
+        this.userRoles = data.userRoles;
+        this.userGroups = data.userGroups;
+    }
 
-//     belongToUserGroup(userGroupUid: string): boolean {
-//         return this.userGroups.some(({ id }) => id === userGroupUid);
-//     }
+    belongToUserGroup(userGroupUid: string): boolean {
+        return this.userGroups.some(({ id }) => id === userGroupUid);
+    }
 
-//     isAdmin(): boolean {
-//         return this.userRoles.some(({ authorities }) => authorities.includes("ALL"));
-//     }
-// }
+    isAdmin(): boolean {
+        return this.userRoles.some(({ authorities }) => authorities.includes("ALL"));
+    }
+}
--- src/domain/entities/__tests__/User.spec.ts
+++ src/domain/entities/__tests__/User.spec.ts
@@ -1,205 +1,70 @@
-import { User } from "../User";
-
-const userNotAdmin = {
-    id: 1,
-    name: "Joellen de Banke",
-    username: "jde0",
-    userRoles: [{ id: "", name: "", authorities: ["Account Representative I"] }],
-    userGroups: [],
-    disabled: false,
-};
-
-const userAdmin = {
-    id: 2,
-    name: "Brigit Brewitt",
-    username: "bbrewitt1",
-    userRoles: [{ id: "", name: "", authorities: ["ALL"] }],
-    userGroups: [],
-    disabled: false,
-};
-
-function GivenANonAdminUser(): User {
-    return new User(userNotAdmin);
-}
-
-function GivenANonAdminUserThatWasDisabledYesterday(): User {
-    const user = new User(userNotAdmin);
-    user.disable(getYesterdayDate());
-    return user;
-}
-
-function GivenAnAdminUser(): User {
-    return new User(userAdmin);
-}
-
-function getYesterdayDate(): Date {
-    return new Date(Date.now() - 864e5);
-}
-
-function getTomorrowDate(): Date {
-    return new Date(Date.now() + 864e5);
-}
+import { User, UserRole } from "../User";
+import { describe, expect, it } from "@jest/globals";
+import { NamedRef } from "../Ref";
 
 describe("User", () => {
-    it("can be disabled if isn't admin", () => {
-        //Given
-        const user = GivenANonAdminUser();
+    it("should be admin if has a role with authority ALL", () => {
+        const user = createAdminUser();
 
-        //Then
-        user.disable();
-
-        //When
-        expect(user.isDisabled()).toBe(true);
+        expect(user.isAdmin()).toBe(true);
     });
-});
+    it("should no be admin if hasn't a role with authority ALL", () => {
+        const user = createNonAdminUser();
 
-describe("User", () => {
-    it("can't be disabled if it is admin", () => {
-        //Given
-        const adminUser = GivenAnAdminUser();
-
-        //Then
-        adminUser.disable();
-
-        //When
-        expect(adminUser.isDisabled()).toBe(false);
-    });
-});
-
-// describe("User", () => {
-//     it("can't be disabled if it is already disabled", () => {
-//         //Given
-//         const user = GivenANonAdminUserThatWasDisabledYesterday();
-
-//         //Then
-
-//         //When
-//         expect(user.disable()).toStrictEqual(getYesterdayDate());
-//     });
-// });
-
-describe("User", () => {
-    it("can't be disabled if given disable date is newer than current date", () => {
-        //Given
-        const user = GivenANonAdminUser();
-
-        //Then
-
-        //When
-        try {
-            user.disable(getTomorrowDate());
-        } catch (error) {
-            expect(error).toHaveProperty(
-                "message",
-                "Date when user was disabled can't be newer than the current date."
-            );
-        }
-    });
-});
-
-describe("User", () => {
-    it("should be admin when user role has an ALL authority", () => {
-        //Given
-        const adminUser = GivenAnAdminUser();
-
-        //Then
-
-        //When
-        expect(adminUser.isAdmin()).toBe(true);
-    });
-});
-
-describe("User", () => {
-    it("shouldn't be admin when user role hasn't an ALL authority", () => {
-        //Given
-        const user = GivenANonAdminUser();
-
-        //Then
-
-        //When
         expect(user.isAdmin()).toBe(false);
     });
-});
+    it("should return belong to user group equal to false when the id exist", () => {
+        const userGroupId = "BwyMfDBLih9";
 
-describe("User", () => {
-    it("should return the days disabled being equal to 1 if date when disabled was yesterday", () => {
-        //Given
-        const user = GivenANonAdminUserThatWasDisabledYesterday();
+        const user = createUserWithGroups([{ id: userGroupId, name: "Group 1" }]);
 
-        //Then
+        expect(user.belongToUserGroup(userGroupId)).toBe(true);
+    });
+    it("should return belong to user group equal to false when the id does not exist", () => {
+        const existedUserGroupId = "BwyMfDBLih9";
+        const nonExistedUserGroupId = "f31IM13BgwJ";
 
-        //When
-        expect(user.daysDisabled()).toEqual(1);
+        const user = createUserWithGroups([{ id: existedUserGroupId, name: "Group 1" }]);
+
+        expect(user.belongToUserGroup(nonExistedUserGroupId)).toBe(false);
+    });
+    it("should return belong to user group equal to false if user groups is empty", () => {
+        const nonExistedUserGroupId = "f31IM13BgwJ";
+
+        const user = createUserWithGroups();
+
+        expect(user.belongToUserGroup(nonExistedUserGroupId)).toBe(false);
     });
 });
 
-// import { User, UserRole } from "../User";
-// import { describe, expect, it } from "@jest/globals";
-// import { NamedRef } from "../Ref";
+function createAdminUser(): User {
+    const adminRoles = [{ id: "Hg7n0MwzUQn", name: "Super user", authorities: ["ALL"] }];
 
-// describe("User", () => {
-//     it("should be admin if has a role with authority ALL", () => {
-//         const user = createAdminUser();
+    return createUser(adminRoles, []);
+}
 
-//         expect(user.isAdmin()).toBe(true);
-//     });
-//     it("should no be admin if hasn't a role with authority ALL", () => {
-//         const user = createNonAdminUser();
+function createNonAdminUser(): User {
+    const nonAdminRoles = [{ id: "Hg7n0MwzUQn", name: "Malaria", authorities: ["F_EXPORT_DATA"] }];
 
-//         expect(user.isAdmin()).toBe(false);
-//     });
-//     it("should return belong to user group equal to false when the id exist", () => {
-//         const userGroupId = "BwyMfDBLih9";
+    return createUser(nonAdminRoles, []);
+}
 
-//         const user = createUserWithGroups([{ id: userGroupId, name: "Group 1" }]);
+function createUserWithGroups(userGroups: NamedRef[] = []): User {
+    return new User({
+        id: "YjJdEO6d38H",
+        name: "Example test",
+        username: "example",
+        userRoles: [],
+        userGroups,
+    });
+}
 
-//         expect(user.belongToUserGroup(userGroupId)).toBe(true);
-//     });
-//     it("should return belong to user group equal to false when the id does not exist", () => {
-//         const existedUserGroupId = "BwyMfDBLih9";
-//         const nonExistedUserGroupId = "f31IM13BgwJ";
-
-//         const user = createUserWithGroups([{ id: existedUserGroupId, name: "Group 1" }]);
-
-//         expect(user.belongToUserGroup(nonExistedUserGroupId)).toBe(false);
-//     });
-//     it("should return belong to user group equal to false if user groups is empty", () => {
-//         const nonExistedUserGroupId = "f31IM13BgwJ";
-
-//         const user = createUserWithGroups();
-
-//         expect(user.belongToUserGroup(nonExistedUserGroupId)).toBe(false);
-//     });
-// });
-
-// function createAdminUser(): User {
-//     const adminRoles = [{ id: "Hg7n0MwzUQn", name: "Super user", authorities: ["ALL"] }];
-
-//     return createUser(adminRoles, []);
-// }
-
-// function createNonAdminUser(): User {
-//     const nonAdminRoles = [{ id: "Hg7n0MwzUQn", name: "Malaria", authorities: ["F_EXPORT_DATA"] }];
-
-//     return createUser(nonAdminRoles, []);
-// }
-
-// function createUserWithGroups(userGroups: NamedRef[] = []): User {
-//     return new User({
-//         id: "YjJdEO6d38H",
-//         name: "Example test",
-//         username: "example",
-//         userRoles: [],
-//         userGroups,
-//     });
-// }
-
-// function createUser(userRoles: UserRole[], userGroups: NamedRef[] = []): User {
-//     return new User({
-//         id: "YjJdEO6d38H",
-//         name: "Example test",
-//         username: "example",
-//         userRoles,
-//         userGroups,
-//     });
-// }
+function createUser(userRoles: UserRole[], userGroups: NamedRef[] = []): User {
+    return new User({
+        id: "YjJdEO6d38H",
+        name: "Example test",
+        username: "example",
+        userRoles,
+        userGroups,
+    });
+}
