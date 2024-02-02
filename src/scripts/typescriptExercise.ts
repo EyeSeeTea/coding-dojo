ba07a7107f56334537e6adb74d2a89f132a22401
davidbgomes <davidbgomes92@gmail.com>
2024-02-02 07:26:05 -0300

    Typescript exercise 1

---
+++ src/scripts/typescriptExercise.ts
@@ -0,0 +1,34 @@
+type NamedObject<T extends string | number> = {
+    [key: string]: T;
+};
+
+function namedToInitials<T extends string | number>(
+    namedObj: NamedObject<T>
+): NamedObject<T | string> {
+    if (!namedObj.name) {
+        return { ...namedObj };
+    }
+
+    const initials: string = (namedObj.name as string)
+        .split(" ")
+        .map((word: string) => word.charAt(0).toUpperCase())
+        .join("");
+
+    delete namedObj.name;
+
+    return { ...namedObj, initials };
+}
+
+// Example usage:
+const result1 = namedToInitials({ id: 1, name: "Kristin Scott Thomas" });
+console.log(result1); // Output: { id: 1,  initials: "KST" }
+
+const result2 = namedToInitials({ code: "M1", name: "Mary Cassat" });
+console.log(result2); // Output: { code: "M1", initials: "MC" }
+
+const result3 = namedToInitials({ id: 2, code: "M2" });
+console.log(result2); // Output: {id: 2, code: "M2"}
+
+type LibraryBookState =
+    | { status: "available" }
+    | { status: "checked out"; dueDate: Date; isOnHold: boolean };
