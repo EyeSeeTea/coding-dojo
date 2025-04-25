df49714e9a1b20ab85d616db7979623b8588920f
Ana Garcia <ana@eyeseetea.com>
2025-04-25 11:00:09 +0200

    Exercise 3

--- src/typescript/ts-exercise3.spec.ts
+++ src/typescript/ts-exercise3.spec.ts
@@ -1,5 +1,5 @@
 /*
-EXERCISE: when and how to use "satisfies"
+EXERCISE: when and how to use "satisfies" --> https://www.typescriptlang.org/docs/handbook/release-notes/typescript-4-9.html
 
 Check at the code below. Currently, `config` is an unrestricted object.
 
@@ -14,9 +14,45 @@ When you're done, reflect on the differences between the different approaches.
 
 type RequestInfo = { url: string; limit: number };
 
-type Config = object;
+// type Config = object;
 
-export const config: Config = {
+// export const config: Config = {
+//     users: { url: "/api/users", limit: 10 },
+//     events: { url: "/api/events", limit: 20 },
+// };
+
+// OPTION 1
+// 1. Define a more restrictive `Config` type.
+type Config = {
+    users: RequestInfo;
+    events: RequestInfo;
+};
+
+// 2. Use the `satisfies` keyword.
+export const config = {
     users: { url: "/api/users", limit: 10 },
     events: { url: "/api/events", limit: 20 },
+} satisfies Config;
+
+// OPTION 2
+// 1. Define a more restrictive `Config` type.
+type ConfigMoreRestrictive = {
+    users: { url: "/api/users"; limit: number };
+    events: { url: "/api/events"; limit: number };
 };
+
+// This is type string
+const _userUrl = config.users.url;
+// This is type string
+const _eventUrl = config.events.url;
+
+// 2. Use the `satisfies` keyword.
+export const configMoreRestrictive = {
+    users: { url: "/api/users", limit: 10 },
+    events: { url: "/api/events", limit: 20 },
+} satisfies ConfigMoreRestrictive;
+
+// This is type "/api/users"
+const _userUrlMoreRestrictive = configMoreRestrictive.users.url;
+// This is type "/api/events"
+const _eventUrlMoreRestrictive = configMoreRestrictive.events.url;
